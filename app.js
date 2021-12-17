816105e6d125484142b0f19096a0630d814108e0
TrevorRezac <trev.rezac@gmail.com>
2021-12-16 16:06:55 -0800

    poll form and vote btns work

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ import { redirectIfLoggedIn, signUpUser, signInUser } from './fetch-utils.js';
 const signUpForm = document.getElementById('sign-up-form');
 const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
-// console.log(signUpForm, signUpEmail, signUpPassword);
+
 const signInForm = document.getElementById('sign-in-form');
 const signInEmail = document.getElementById('sign-in-email');
 const signInPassword = document.getElementById('sign-in-password');
-console.log(signInForm, signInEmail, signInPassword);
+
 signUpForm.addEventListener('submit', async(e) => {
     e.preventDefault();
 
--- polls/polls.js
+++ polls/polls.js
@@ -1,7 +1,53 @@
 // import functions and grab DOM elements
-import { logout } from '../fetch-utils.js';
+import { checkAuth, logout } from '../fetch-utils.js';
+
+checkAuth();
 
 const logoutBtn = document.getElementById('logout-btn');
+
+const pollForm = document.getElementById('poll-form');
+const pollQuestionEl = document.getElementById('poll-question');
+const optionATitleEl = document.getElementById('option-a-title');
+const optionAVotesEl = document.getElementById('option-a-votes');
+const optionBTitleEl = document.getElementById('option-b-title');
+const optionBVotesEl = document.getElementById('option-b-votes');
+const voteABtn = document.getElementById('vote-a-btn');
+const voteBBtn = document.getElementById('vote-b-btn');
+const closePollBtn = document.getElementById('close-poll-btn');
+
+
+let question = '';
+let optionA = '';
+let optionB = '';
+let votesA = 0;
+let votesB = 0;
+
+
+pollForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const data = new FormData(pollForm);
+
+    const question = data.get('poll-question');
+    const optionA = data.get('option-a');
+    const optionB = data.get('option-b');
+
+    pollQuestionEl.textContent = question;
+    optionATitleEl.textContent = optionA;
+    optionBTitleEl.textContent = optionB;
+
+    pollForm.reset();
+});
+
+voteABtn.addEventListener('click', () => {
+    votesA++;
+    optionAVotesEl.textContent = votesA;
+});
+
+voteBBtn.addEventListener('click', () => {
+    votesB++;
+    optionBVotesEl.textContent = votesB;
+});
 
 logoutBtn.addEventListener('click', () => {
     logout();
